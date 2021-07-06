85ab104a359900136d75339c1bb588d68b75d0dc
Iamabayomi <Oladinifolawiyo@gmail.com>
2021-07-06 20:03:04 +0100

    added the website into brackets for everyone

--- src/components/headline.js
+++ src/components/headline.js
@@ -3,10 +3,19 @@ import React from 'react'
 export default class Headline extends React.Component  {
     constructor(props) {
         super(props)
+        this.headlineNo = 1
+        this.headlineUrl = ''
         console.log('This is the headline in the component' , this.props.headlines)
         // console.log('This is the headline in the component' , props.headlines)
     }
     
+    getWebsiteDomain(headlineUrl){
+        if(headlineUrl){
+            headlineUrl =headlineUrl.split('/')
+            return `(${headlineUrl[2]})`
+        }
+        return ''
+    }
     
     render(){
     console.log('This is the headline in the component' , this.props.headlines)
@@ -14,17 +23,19 @@ export default class Headline extends React.Component  {
         <div>
             {this.props.headlines.map((headline) =>(
             <div class="headline">
+                {}
                 <div class="headline-no">
-                    <p>1..</p> 
+                    <p>{this.headlineNo++}</p> 
                     <div class="grayarrow" title="upvote"></div>
                 </div>
                 <div class="headline-content">
                     <div class="headline-section">
-                        <p class="main-headline">
+                        <a href={headline.url} class="main-headline link-style"
+                         target="_blank" rel="noreferrer" >
                             {headline.title}
-                        </p>
+                        </a>
                         <p class="headline-website">
-                            (news.mit.edu)
+                            { this.getWebsiteDomain(headline.url) }
                         </p>
                     </div>
                     <div class="headline-comments">
